7bc167041c5b67448ccdfa3f56da9eb41dae4796
alf <alf@hubmed.org>
2010-07-30 20:15:10 +0800

    added Amazon lookup for the current track

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
 <?php
-include('simplepie/simplepie.inc');
 
-$feed = new SimplePie();
+include __DIR__ . '/config.php';
+require __DIR__ . '/simplepie/simplepie.inc';
+
+$feed = new SimplePie;
 $feed->set_feed_url("http://ws.audioscrobbler.com/1.0/user/bbc6music/recenttracks.rss");
-$feed->set_cache_duration(180); # 3 minutes
+$feed->set_cache_duration(59); # 1 minute
 $feed->init();
 $feed->handle_content_type();
 
 $songs = $feed->get_items();
+$song = $songs[0]->get_title();
+$query = preg_replace('/\W/', ' ', $song);
+
+$mp3s = array();
+if ($conf['amazon']['key'] && $conf['amazon']['secret']) {
+  require __DIR__ . '/amazon.php';
+  $amazon = new Amazon;
+  $amazon->key = $conf['amazon']['key'];
+  $amazon->secret = $conf['amazon']['secret'];
+  $amazon->tag = $conf['amazon']['tag'];
+  $mp3s = $amazon->lookup($query);
+}
+
+if (empty($mp3s))
+  $mp3s = array(array('info' => 'http://www.amazon.com/s/?url=search-alias%3Ddigital-music&field-keywords=' . urlencode($query)));
 
 $feed->set_feed_url("http://twitter.com/statuses/user_timeline/15734589.rss");
-$feed->set_cache_duration(180); # 3 minutes
+$feed->set_cache_duration(59); # 1 minute
 $feed->init();
 $feed->handle_content_type();
 
@@ -49,7 +66,7 @@ function plural($num) {
   <title>6music - what's playing</title>
   <link rel="icon" href="favicon.ico" type="image/x-icon">
   <meta name="viewport" content="width=device-width">
-  <!-- <meta http-equiv="refresh" content="120" /> --> <!-- reload page every two minutes -->
+  <meta http-equiv="refresh" content="60" /> <!-- reload page every minute -->
   <style>
   body {
     text-align: center;
@@ -66,27 +83,12 @@ function plural($num) {
   }
   </style>
 
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-65232-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-
 </head>
 <body>
-  <p id="song"><?php echo $songs[0]->get_title(); ?></p>
-  <p>played <?php echo getRelativeTime($songs[0]->get_date('U')); ?> by</p>
-  <p id="show"><a href="http://www.bbc.co.uk/programmes/<?php echo preg_replace ('/[^a-z0-9 ]/i', '', $showtitle); ?>"><?php echo $showtitle; ?></a></p>
-  <!-- <p id="listen"><a href="http://www.bbc.co.uk/iplayer/console/bbc_6music">listen live</a></p> -->
-  <p id="refresh"><a href="javascript:window.location.reload();"><img src="01-refresh.png" width="24" height="26" border="0" alt="reload this page"></a></p>
+  <p id="song"><a href="<?= htmlspecialchars($mp3s[0]['info']); ?>"><?= htmlspecialchars($song); ?></a></p>
+  <p>played <?= htmlspecialchars(getRelativeTime($songs[0]->get_date('U'))); ?> by</p>
+  <p id="show"><a href="http://www.bbc.co.uk/programmes/<?= htmlspecialchars(preg_replace ('/[^a-z0-9 ]/i', '', $showtitle)); ?>"><?= htmlspecialchars($showtitle); ?></a></p>
   <p id="about"><a href="about.html">about</a></p>
 </body>
 </html>
+
